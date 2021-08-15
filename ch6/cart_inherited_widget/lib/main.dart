bc6c6f5d97078edf79c4da7e818781b8041606a3
wal33d006 <waleed_arshad006@hotmail.com>
2021-08-15 13:28:19 +0500

    Update app bar title. Refactor code

--- ch6/cart_inherited_widget/lib/main.dart
+++ ch6/cart_inherited_widget/lib/main.dart
@@ -41,52 +41,51 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: ListView(
-        children: MyCartInheritedWidget.of(context)
-            .items
+        children: items
             .map(
               (e) => ListTile(
-            title: Text(e.name ?? ''),
-            subtitle: Text("USD " + (e.price ?? '')),
-            trailing: IconButton(
-              icon: Icon(
-                cart.contains(e) ? Icons.remove_circle : Icons.add_circle,
+                title: Text(e.name ?? ''),
+                subtitle: Text("USD " + (e.price ?? '')),
+                trailing: IconButton(
+                  icon: Icon(
+                    cart.contains(e) ? Icons.remove_circle : Icons.add_circle,
+                  ),
+                  onPressed: () {
+                    if (!cart.contains(e))
+                      MyCartInheritedWidget.of(context).addToCart(e);
+                    else
+                      MyCartInheritedWidget.of(context).removeFromCart(e);
+                  },
+                ),
               ),
-              onPressed: () {
-                if (!cart.contains(e))
-                  MyCartInheritedWidget.of(context).addToCart(e);
-                else
-                  MyCartInheritedWidget.of(context).removeFromCart(e);
-              },
-            ),
-          ),
-        )
+            )
             .toList(),
       ),
       floatingActionButton: items.isEmpty
           ? null
           : FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => CartPage(),
-            ),
-          );
-        },
-        label: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.red,
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => CartPage(),
+                  ),
+                );
+              },
+              label: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                    child: Text(cart.length.toString()),
+                  ),
+                  SizedBox(width: 8),
+                  Text('Cart'),
+                ],
               ),
-              child: Text(cart.length.toString()),
             ),
-            SizedBox(width: 8),
-            Text('Cart'),
-          ],
-        ),
-      ),
     );
   }
 }
--- ch6/cart_set_state/lib/main.dart
+++ ch6/cart_set_state/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Cart - setState'),
     );
   }
 }
