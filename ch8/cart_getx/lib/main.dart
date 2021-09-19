3017587501c823920c0c63dbc1b435c105793d40
wal33d006 <waleed_arshad006@hotmail.com>
2021-09-19 23:20:10 +0500

    Add steps as comments

--- ch8/cart_getx/lib/main.dart
+++ ch8/cart_getx/lib/main.dart
@@ -33,59 +33,67 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // One
     Get.put(CartController());
-    return GetBuilder(builder: (CartController controller) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: controller.cart.value.items
-              .map(
-                (e) => ListTile(
-                  title: Text(e.name ?? ''),
-                  subtitle: Text("USD " + (e.price ?? '')),
-                  trailing: IconButton(
-                    icon: Icon(
-                      controller.cart.value.cart.contains(e) ? Icons.remove_circle : Icons.add_circle,
+    // Two
+    return GetBuilder(
+      builder: (CartController controller) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: ListView(
+            // Three
+            children: controller.cart.value.items
+                .map(
+                  (e) => ListTile(
+                    title: Text(e.name ?? ''),
+                    subtitle: Text("USD " + (e.price ?? '')),
+                    trailing: IconButton(
+                      icon: Icon(
+                        controller.cart.value.cart.contains(e) ? Icons.remove_circle : Icons.add_circle,
+                      ),
+                      onPressed: () {
+                        // Four
+                        if (!controller.cart.value.cart.contains(e))
+                          controller.addItemToCart(e);
+                        else
+                          controller.removeItemFromCart(e);
+                      },
                     ),
-                    onPressed: () {
-                      if (!controller.cart.value.cart.contains(e))
-                        controller.addItemToCart(e);
-                      else
-                        controller.removeItemFromCart(e);
-                    },
+                  ),
+                )
+                .toList(),
+          ),
+          // Five
+          floatingActionButton: controller.cart.value.cart.isEmpty
+              ? null
+              : FloatingActionButton.extended(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CartPage(),
+                      ),
+                    );
+                  },
+                  label: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                        // Six
+                        child: Text(controller.cart.value.cart.length.toString()),
+                      ),
+                      SizedBox(width: 8),
+                      Text('Cart'),
+                    ],
                   ),
                 ),
-              )
-              .toList(),
-        ),
-        floatingActionButton: controller.cart.value.cart.isEmpty
-            ? null
-            : FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => CartPage(),
-                    ),
-                  );
-                },
-                label: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.red,
-                      ),
-                      child: Text(controller.cart.value.cart.length.toString()),
-                    ),
-                    SizedBox(width: 8),
-                    Text('Cart'),
-                  ],
-                ),
-              ),
-      );
-    });
+        );
+      },
+    );
   }
 }
